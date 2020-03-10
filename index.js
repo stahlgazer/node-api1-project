9cb94c1c1ed5e486146568987d6052e6a789276f
gavin stahl <stahlgavin@gmail.com>
2020-03-09 20:48:19 -0700

    server running

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express"); // npm i express
 const shortid = require("shortid");
 const server = express();
 
+const PORT = 5000;
+server.listen(PORT, () =>
+console.log(`\n** API on http://localhost:${PORT} **\n`))
+
+server.use(express.json()); // add this line
+// to run the server use node index.js ??
+
 let users = [
     {
         id: 0,
@@ -9,8 +16,6 @@ let users = [
         bio: "Not Tarzan's Wife, another Jane",
       }
 ]
-
-server.use(express.json()); // add this line
 
 server.get('/', (req, res) => {
     res.status(200).json({api: 'running...'})
